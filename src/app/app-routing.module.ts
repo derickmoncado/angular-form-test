ba2f489c8a3601936a0985ba242dfce6e6cd2355
Derick Moncado <derickmoncado@gmail.com>
2021-05-04 12:26:40 -0700

    Generated app, cleared out boilerplate, set up components and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { FirstFormComponent } from './components/first-form/first-form.component';
+import { SecondFormComponent } from './components/second-form/second-form.component';
+import { ThirdFormComponent } from './components/third-form/third-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'first-form', component: FirstFormComponent },
+  { path: 'second-form', component: SecondFormComponent },
+  { path: 'third-form', component: ThirdFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstFormComponent } from './components/first-form/first-form.component';
+import { SecondFormComponent } from './components/second-form/second-form.component';
+import { ThirdFormComponent } from './components/third-form/third-form.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstFormComponent,
+    SecondFormComponent,
+    ThirdFormComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
